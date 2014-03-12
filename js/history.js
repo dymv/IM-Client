797d4b885d2c460ee233e6c044c80a6543e81bf8
yodairish <yodairish@gmail.com>
2014-03-11 23:59:53 -0400

    history

--- js/history.js
+++ js/history.js
@@ -1,10 +1,26 @@
 function History() {
 }
 
-History.prototype.save = function() {
+History.prototype.save = function(newMessage) {
+  if (!imchat.utils.isMsgObj(newMessage)) throw 'incorrect new message';
+  
+  if (sessionStorage) {
+    var messages = this.load();
+    messages.push(newMessage);
+    sessionStorage.setItem('messages', JSON.stringify(messages));
+  }
 };
 
 History.prototype.load = function() {
+  var messages = [];
+  
+  if (sessionStorage && sessionStorage.getItem('messages') !== null) {
+    try {
+      messages = JSON.parse(sessionStorage.messages);
+    } catch(error){}
+  }
+  
+  return messages;
 };
 
 namespace('imchat.history', History);
--- tests/history.js
+++ tests/history.js
@@ -1,1 +1,54 @@
 module('History ====');
+test('save', function(){
+  var tmpSave = sessionStorage.messages;
+  
+  throws(
+    function() { imchat.history.save(); }, "without params"
+  );
+  
+  throws(
+    function() { imchat.history.save(123); }, "number"
+  );
+  
+  throws(
+    function() { imchat.history.save({some:123, an: 'asd'}); }, "incorrect obj"
+  );
+  
+  throws(
+    function() {
+      imchat.history.save({message: 12, author: 'some', time: 123});
+    }, "error in message object"
+  );
+  
+  var obj = {message: 'some', author: 'some', time: 123};
+  
+  // mock ...
+  
+  sessionStorage.removeItem('messages');
+  imchat.history.save(obj);
+  strictEqual(sessionStorage.getItem('messages'), JSON.stringify([obj]),
+            'one correct object');
+  
+  sessionStorage.removeItem('messages');
+  imchat.history.save(obj);
+  imchat.history.save(obj);
+  strictEqual(sessionStorage.getItem('messages'), JSON.stringify([obj, obj]),
+            'two correct object');
+  
+  sessionStorage.messages = tmpSave;
+});
+
+test('load', function(){
+  var tmpSaveBag = sessionStorage.messages;
+  
+  sessionStorage.removeItem('messages');
+  deepEqual(imchat.history.load(), [], 'no messages');
+  
+  sessionStorage.messages = 'asdad';
+  deepEqual(imchat.history.load(), [], 'incorrect value in messages');
+  
+  sessionStorage.messages = JSON.stringify([1, 2]);
+  deepEqual(imchat.history.load(), [1, 2], 'some messages');
+  
+  sessionStorage.messages = tmpSaveBag
+});
